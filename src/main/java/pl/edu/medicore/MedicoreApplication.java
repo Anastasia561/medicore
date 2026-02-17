72c65e328be0c059906b99d1eca2e55c49a9c1d7
Anastasia561 <kurulenkoanastasia@gmail.com>
2026-02-17 15:03:59 +0100

    Initial commit

---
+++ src/main/java/pl/edu/medicore/MedicoreApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.medicore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicoreApplication.class, args);
+    }
+
+}
