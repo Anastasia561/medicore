b410000bab195f5dc3545d9f245c5618ee561d70
Anastasia561 <kurulenkoanastasia@gmail.com>
2026-03-25 17:32:23 +0100

    feat: add lab test tables

--- flyway/sql/schema/V3__create_verification_token_table.sql
+++ flyway/sql/schema/V3__create_verification_token_table.sql
@@ -1,14 +1,10 @@
+-- Table: verification_token
 CREATE TABLE verification_token
 (
     id         BIGSERIAL PRIMARY KEY,
-
-    token_hash TEXT       NOT NULL,
-
-    token_type varchar(40) NOT NULL,
-
+    token_hash TEXT         NOT NULL,
+    token_type varchar(40)  NOT NULL,
     email      VARCHAR(100) NOT NULL,
-
-    expires_at TIMESTAMP  NOT NULL,
-
-    created_at TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP
+    expires_at TIMESTAMP    NOT NULL,
+    created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
