3638dc47ba7176f6a9969956eccf9de196919beb
Anastasia561 <kurulenkoanastasia@gmail.com>
2026-08-01 16:49:36 +0200

    feat: switch to localstack for local dev env'

--- medicore-api/src/main/java/pl/edu/medicore/common/config/AWSConfig.java
+++ medicore-api/src/main/java/pl/edu/medicore/common/config/AWSConfig.java
@@ -8,27 +8,63 @@ import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3ClientBuilder;
+import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+import java.net.URI;
 
 @Configuration
 @RequiredArgsConstructor
 @Profile("!test")
 public class AWSConfig {
+
     @Value("${app.aws.s3.region}")
     private String region;
 
+    @Value("${app.aws.s3.endpoint:}")
+    private String endpoint;
+
+    @Value("${app.aws.s3.public-endpoint:}")
+    private String publicEndpoint;
+
     @Bean
-    S3Presigner s3Presigner() {
-        return S3Presigner.builder()
+    public S3Presigner s3Presigner() {
+        S3Presigner.Builder builder = S3Presigner.builder()
                 .region(Region.of(region))
-                .credentialsProvider(DefaultCredentialsProvider.builder().build())
-                .build();
+                .credentialsProvider(DefaultCredentialsProvider.create());
+
+        String presignerEndpoint = publicEndpoint;
+        if (presignerEndpoint == null || presignerEndpoint.isBlank()) {
+            presignerEndpoint = endpoint;
+        }
+
+        if (presignerEndpoint != null && !presignerEndpoint.isBlank()) {
+            builder.endpointOverride(URI.create(presignerEndpoint))
+                    .serviceConfiguration(
+                            S3Configuration.builder()
+                                    .pathStyleAccessEnabled(true)
+                                    .build()
+                    );
+        }
+
+        return builder.build();
     }
 
     @Bean
     public S3Client s3Client() {
-        return S3Client.builder()
-                .region(Region.of(region))
-                .build();
+        S3ClientBuilder builder = S3Client.builder()
+                .region(Region.of(region));
+
+        if (endpoint != null && !endpoint.isBlank()) {
+            builder.endpointOverride(URI.create(endpoint))
+                    .serviceConfiguration(
+                            S3Configuration.builder()
+                                    .pathStyleAccessEnabled(true)
+                                    .build()
+                    );
+        }
+
+        return builder.build();
     }
 }
